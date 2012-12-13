f80615cf04c906e683e4626582f2abe92ed4e347
antho1404 <anthony.estebe@gmail.com>
2012-12-13 18:42:34 +0100

    use pipeline in generated files and add script to the app.js file

--- lib/generators/mailchecker/assets_generator.rb
+++ lib/generators/mailchecker/assets_generator.rb
@@ -6,7 +6,15 @@ module Mailchecker
       desc "Create the javascript file to make the validation in client side."
 
       def copy_javascript
-        template "assets/mailchecker.js.coffee.erb", "app/assets/javascripts/mailchecker_client.js.coffee.erb"
+        copy_file "assets/mailchecker.js.coffee.erb", "app/assets/javascripts/mailchecker_client.js.coffee.erb"
+        
+        if File.exist?('app/assets/javascripts/application.js')
+          insert_into_file "app/assets/javascripts/application.js", "//= require mailchecker_client\n", after: "jquery_ujs\n"
+        else
+          if File.exist?('app/assets/javascripts/application.js.coffee')
+            insert_into_file "app/assets/javascripts/application.js.coffee", "#= require mailchecker_client\n", after: "jquery_ujs\n"
+          end
+        end
       end
     end
   end
